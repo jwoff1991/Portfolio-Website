b44daf867a33a54da7e6004257f0d1ec0ce59bb6
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-07-26 00:12:49 +0200

    got projects page up with a few projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,21 @@
+import ProjectItem from "./ProjectItem";
+import '../styles/Projects.css'
+import { ProjectList } from "../projectData/ProjectList";
+
 function Projects() {
+  const Projects = ProjectList.map(project => {
+
+    return (
+      <ProjectItem name={project.name} image={project.image}/>
+    )
+  })
+
   return (
     <div className='projects'>
       <h1>My Personal Projects</h1>
-      <div className ='projectList'></div>
+      <div className ='projectList'>
+        {Projects}
+      </div>
     </div>
   );
 }
---
+++ src/projectData/ProjectList.js
@@ -0,0 +1,16 @@
+import Firstwebsite from '../assets/firstwebsite.png'
+
+export const ProjectList = [
+    {
+        name: "ErrBnB",
+        image: null
+    },
+    {
+        name: "filker Clone",
+        image: null
+    },
+    {
+        name: "First Website",
+        image: Firstwebsite
+    }
+]
