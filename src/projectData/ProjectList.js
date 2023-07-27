09d79f7a808d1ef58876ee843430f02569e2d15e
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-07-27 06:44:36 +0200

    fixed minor syntax error

--- src/projectData/ProjectList.js
+++ src/projectData/ProjectList.js
@@ -2,32 +2,33 @@ import Firstwebsite from '../assets/firstwebsite.png'
 import flickerClone from '../assets/flickerclone.png'
 
 export const ProjectList = [
-    {
-        id: 0,
-        name: "ErrBnB",
-        image: null,
-        alt: "null image",
-        skills: 'Javascript, SQL, PostgreSQL, React, Redux, HTML, CSS, Postman, Node',
-        gitHub: 'https://github.com/jwoff1991/API-project-ErrBnB',
-        details: 'Fullstack solo project. Designed, coded, tested and deployed both the backend and frontend.'
-    },
-    {
-        id: 1,
-        name: "flickr Clone",
-        image: flickerClone,
-        alt: "flicker website front page clone",
-        skills: 'HTML/CSS',
-        gitHub: 'https://github.com/jwoff1991/flickr-clone',
-        details: 'HTML/CSS front end project based on "wireframes".'
-    },
-    {
-
-        id: 2,
-        name: "First Website",
-        image: Firstwebsite,
-        alt: "Picture of front page of my first website"
-        skills: 'HTML, CSS',
-        gitHub: 'https://github.com/jwoff1991/firstwebsite',
-        details: 'First full website coding in HTML/CSS. '
-    }
-]
+  {
+    id: 0,
+    name: "ErrBnB",
+    image: null,
+    alt: "null image",
+    skills:
+      "Javascript, SQL, PostgreSQL, React, Redux, HTML, CSS, Postman, Node",
+    gitHub: "https://github.com/jwoff1991/API-project-ErrBnB",
+    details:
+      "Fullstack solo project. Designed, coded, tested and deployed both the backend and frontend.",
+  },
+  {
+    id: 1,
+    name: "flickr Clone",
+    image: flickerClone,
+    alt: "flicker website front page clone",
+    skills: "HTML/CSS",
+    gitHub: "https://github.com/jwoff1991/flickr-clone",
+    details: 'HTML/CSS front end project based on "wireframes".',
+  },
+  {
+    id: 2,
+    name: "First Website",
+    image: Firstwebsite,
+    alt: "Picture of front page of my first website",
+    skills: "HTML, CSS",
+    gitHub: "https://github.com/jwoff1991/firstwebsite",
+    details: "First full website coding in HTML/CSS. ",
+  },
+];
