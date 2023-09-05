cdd330994c4ea8641c102501bfcb571f0db17e41
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-05 15:13:54 +0200

    added awn to portfolio

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,12 +1,25 @@
 import { Link, useParams } from "react-router-dom";
 import { ProjectList } from "../projectData/ProjectList";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LanguageIcon from '@mui/icons-material/Language';
+import LinkIcon from '@mui/icons-material/Link';
+
 
 
 
 function ProjectDisplay({ image, name }) {
     const { id } = useParams()
     const project = ProjectList[id]
+
+    let externalLink;
+    if(project && project.link) {
+      externalLink =
+        <Link to={project.link} name="Check out the live site!">
+          <LanguageIcon />
+        </Link>
+    }
+
+
     return (
       <div className="project">
         <h1>{project.name}</h1>
@@ -22,6 +35,7 @@ function ProjectDisplay({ image, name }) {
         >
           <GitHubIcon />
         </Link>
+        {externalLink}
       </div>
     );
   }
--- src/projectData/ProjectList.js
+++ src/projectData/ProjectList.js
@@ -1,21 +1,35 @@
 import Firstwebsite from '../assets/firstwebsite.png'
 import flickerClone from '../assets/flickerclone.png'
 import errBnBwebsite from '../assets/errBnBwebsite.png'
+import AWNhomepage from '../assets/AWN homepage.png'
 
 export const ProjectList = [
   {
     id: 0,
-    name: "ErrBnB",
-    image: errBnBwebsite,
-    alt: "null image",
+    name: "Apocalypse Weather Now",
+    image: AWNhomepage,
+    alt: "Frontpage of AWN website",
     skills:
-      "Javascript, SQL, PostgreSQL, React, Redux, HTML, CSS, Postman, Node",
-    gitHub: "https://github.com/jwoff1991/API-project-ErrBnB",
+      "Javascript, Python, Flask, SQAlchemy, PostgreSQL, React, Redux, HTML, CSS, Node",
+    gitHub: "https://github.com/SonQHoang/Apocalypse-Weather-Now",
     details:
-      "Fullstack solo project. Designed, coded, tested and deployed both the backend and frontend.",
+      "Fullstack group project. As a team we developed our website from scratch utilizing our skills in JS frontend and Python backend. The site has 3 full and 2 partial CRUD features. Link is below! ",
+    link: "https://awn-render.onrender.com/"
   },
   {
     id: 1,
+    name: "ErrBnB",
+    image: errBnBwebsite,
+    alt: "frontpage of ErrBnB website",
+    skills:
+      "Javascript, SQL, PostgreSQL, React, Redux, HTML, CSS, Node",
+    gitHub: "https://github.com/jwoff1991/API-project-ErrBnB",
+    details:
+      "Fullstack solo project. Designed, coded, tested and deployed both the backend and frontend.",
+    link: "https://errbnb.onrender.com/"
+  },
+  {
+    id: 2,
     name: "flickr Clone",
     image: flickerClone,
     alt: "flicker website front page clone",
@@ -24,7 +38,7 @@ export const ProjectList = [
     details: 'HTML/CSS front end project based on "wireframes".',
   },
   {
-    id: 2,
+    id: 3,
     name: "First Website",
     image: Firstwebsite,
     alt: "Picture of front page of my first website",
