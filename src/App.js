79876573d2ae70784c8adddf0b87e46ffe1fe46e
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-22 17:59:41 +0200

    working on css and spelling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navigation from "./components/Navigation";
 import { Routes, Route } from "react-router-dom/";
 import Home from './components/Home'
 import Projects from "./components/Projects";
-import Experience from "./components/Experience";
 import './index.css'
 import Footer from "./components/Footer";
 import ProjectDisplay from "./components/ProjectDisplay";
