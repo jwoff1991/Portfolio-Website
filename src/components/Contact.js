b741be895fcd94f561802d6b61a56c88d186fb78
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-02-22 17:40:06 +0100

    minor adjustment to syntax

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,14 +8,14 @@ function Contact() {
         <div className="contact-div">
             <h1>Contact</h1>
             Enjoyed my work? Have any questions? Just wanna connect? Feel free to reach out to me on any of the socials below:
-            <br></br>
+            <br />
             <div className="contat-socials-div">
             GitHub: <Link to='https://github.com/jwoff1991' target='_blank'>https://github.com/jwoff1991 </Link>
-            <br></br>
+            <br />
             LinkedIn: <Link to="https://www.linkedin.com/in/jonathanbwofford/" target="_blank">https://www.linkedin.com/in/jonathanbwofford/</Link>
-            <br></br>
+            <br />
             Email:
-            <br></br>
+            <br />
             jonathanbrodie91@gmail.com
             </div>
         </div>
